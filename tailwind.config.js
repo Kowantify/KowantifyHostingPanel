87dff5098f03c3b176645d5328915bdb3f8e044c
Kyle Kowalski <153790092+Kowantify@users.noreply.github.com>
2024-01-15 14:42:50 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,27 +16,27 @@ module.exports = {
                 primary: colors.blue,
                 // "neutral" is used for text colors!!! (has to be tailwind variable)
                 neutral: {
-		    // '50' modifies footer text color before being hovered over
-   		    50: '#ffffff',
-		    // '100' modifies header text color before being hovered over
+		            // '50' modifies footer text color before being hovered over
+   		            50: '#ffffff',
+		            // '100' modifies header text color before being hovered over
                     100: '#ffffff',
-		    // '200' modifies text and box highlight
+		            // '200' modifies text and box highlight
                     200: '#ffffff',
                     // '300' modifies header apps colors and footer highlight
                     300: '#ffffff',
-		    // '400' modifies hover highlight of boxes and statistic color of servers
+		            // '400' modifies hover highlight of boxes and statistic color of servers
                     400: '#ffffff',
                     // '500' modifies highlight of boxes before being hovered over
                     500: '#ffffff',
                     // '600' modifies text of login footer, bottom of statistics and input boxes for user network input / api
-		    600: '#b03838',
+		            600: '#b03838',
                     // '700' modifies account boxes
                     700: '#403030',
-	  	    // '800' modifies text of user in login page
+	  	            // '800' modifies text of user in login page
                     800: '#ffffff',
- 		    // '900' modifies login page background
+ 		            // '900' modifies login page background
                     900: '#312121',
-      		    // '950' TESTING BLUE
+      		        // '950' TESTING BLUE
                     950: '#0000ff',
                     },
                 cyan: colors.cyan,            },
