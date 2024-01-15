903c5c349858d610f7a4d975cfaa4d3e66bafdc9
Kyle Kowalski <kowantify@ibytegaming.com>
2024-01-15 17:37:22 -0600

    Changed back to default

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,33 +13,10 @@ module.exports = {
                 slate: colors.slate,
                 // "primary" and "neutral" are deprecated, prefer the use of "blue" and "gray"
                 // in new code.
-                primary: colors.blue,
+                primary: colors.red,
                 // "neutral" is used for text colors!!! (has to be tailwind variable)
-                neutral: {
-		            // '50' modifies footer text color before being hovered over
-   		            50: '#ffffff',
-		            // '100' modifies header text color before being hovered over
-                    100: '#ffffff',
-		            // '200' modifies text and box highlight
-                    200: '#ffffff',
-                    // '300' modifies header apps colors and footer highlight
-                    300: '#ffffff',
-		            // '400' modifies hover highlight of boxes and statistic color of servers
-                    400: '#ffffff',
-                    // '500' modifies highlight of boxes before being hovered over
-                    500: '#ffffff',
-                    // '600' modifies text of login footer, bottom of statistics and input boxes for user network input / api
-		            600: '#b03838',
-                    // '700' modifies account boxes
-                    700: '#403030',
-	  	            // '800' modifies text of user in login page
-                    800: '#ffffff',
- 		            // '900' modifies login page background
-                    900: '#312121',
-      		        // '950' TESTING BLUE
-                    950: '#0000ff',
-                    },
-                cyan: colors.cyan,            },
+                neutral: colors.slate,
+                cyan: colors.rose,            },
             fontSize: {
                 '2xs': '0.625rem',
             },
